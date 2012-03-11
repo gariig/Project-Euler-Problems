4f73a0e7ab8c429ddf75a4212c110f653877695f
gariig <mike@ninelinesofcode.com>
2012-03-11 18:35:13 -0400

    Finished Problem 1

---
+++ ProjectEulerProblems/Problem1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEulerProblems
+{
+    class Problem1
+    {
+        public static int SumMultipleOf3or5(int end)
+        {
+            var range = Enumerable.Range(1, end);
+            return range.Where(i => i % 3 == 0 || i % 5 == 0).Sum();
+        }
+    }
+}
--- ProjectEulerProblems/Program.cs
+++ ProjectEulerProblems/Program.cs
@@ -9,6 +9,9 @@ namespace ProjectEulerProblems
     {
         static void Main(string[] args)
         {
+            int sum = Problem1.SumMultipleOf3or5(999);
+            Console.WriteLine("The sum is {0}", sum);
+            Console.ReadKey();
         }
     }
 }
